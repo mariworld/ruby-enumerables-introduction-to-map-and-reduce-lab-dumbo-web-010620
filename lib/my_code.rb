ac4bad14deb3ee8b2f02252ca4cef809d9e7150d
Demarius Edwards <demarius.edwards@gmail.com>
2019-12-12 04:37:51 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -17,7 +17,7 @@ def map_to_square(source_array)
 end
 
 def reduce_to_total(source_array,starting_point = 0)
-   source_array.reduce(starting_point) {|i,num| i 
+   source_array.reduce(starting_point,:+) {|i,num| i 
    + num}
    #binding.pry
 end
