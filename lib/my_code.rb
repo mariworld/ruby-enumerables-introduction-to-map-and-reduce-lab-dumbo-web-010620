767b07ca441ec5e637d870e754bc52fee221dfe7
Demarius Edwards <demarius.edwards@gmail.com>
2019-12-12 04:25:17 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -21,11 +21,6 @@ def reduce_to_total(source_array)
    #binding.pry
 end
 
-def reduce_to_total(source_array)
-   source_array.reduce()
-   #binding.pry
-end
-
 def reduce_to_all_true(source_array)
    source_array.reduce { |i,a| a && i = true}
 end
